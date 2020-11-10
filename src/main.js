14b377aaeeb90baa42852aad71435d6b22c47bbe
Logan <logan@Logans-MacBook.local>
2020-11-09 23:48:44 -0500

    added apollo, fixed vulns

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Navbar from 'buefy/src/components/navbar'
+import { createProvider } from './vue-apollo'
 // import Buefy from 'buefy'
 
 // Vue.use(Navbar)
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
